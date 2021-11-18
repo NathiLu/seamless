f79ce460a985d06a1474dfa76b696a6843669151
Nils Tonnätt <nils.tonnaett@posteo.de>
2021-11-18 15:40:56 +0100

    [WebControl] Remove authentication

--- ShowControl/webcontrol/__init__.py
+++ ShowControl/webcontrol/__init__.py
@@ -34,9 +34,6 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
-    from . import auth
-    app.register_blueprint(auth.bp)
-
     from . import showcontrol
     app.register_blueprint(showcontrol.bp)
     app.add_url_rule('/', endpoint='index')
